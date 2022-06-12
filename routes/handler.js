b731f855c7c18942309226aba4ff2605fba85897
Roahn <72019287+Roahn@users.noreply.github.com>
2022-06-12 12:23:59 +0530

    created /DashData route to get data from database and send to frontend

--- routes/handler.js
+++ routes/handler.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 const Schema = require('../models/Schema');
 
@@ -36,7 +35,7 @@ router.get('/tweets', (req, res) => {
   res.end(JSON.stringify(str));
 });
 
-router.post('/addTweet', async (req, res) => {
+router.post('/addOrder', async (req, res) => {
   obj = {
     Address: '',
     FNAME: '',
@@ -46,7 +45,7 @@ router.post('/addTweet', async (req, res) => {
   };
   obj.Address = req.body.address;
   obj.FNAME = req.body.fname;
-  obj.LNAME = req.body.Eggs;
+  obj.LNAME = req.body.lname;
   obj.COUNT = req.body.Eggs;
   obj.PRICE = req.body.EggsPrice;
   console.log(obj);
@@ -54,14 +53,18 @@ router.post('/addTweet', async (req, res) => {
   try {
     await newOrder.save(async (err, newOrderResult) => {
       console.log('New user Created: ' + JSON.stringify(newOrderResult));
-
-    
     });
   } catch (err) {
     console.log(err);
-    
   }
   res.redirect('/');
 });
 
+router.get('/DashData',  async(req, res) => {
+  
+    const data = Schema.Order;
+    const OrderData = await data.find().exec();
+    res.end(JSON.stringify(OrderData));
+  
+});
 module.exports = router;
